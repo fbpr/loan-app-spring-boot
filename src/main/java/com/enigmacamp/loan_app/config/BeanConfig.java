e3296dddf30c38adb4be897369c7f5f4eb771955
fbpr <71777049+fbpr@users.noreply.github.com>
2025-02-25 07:46:12 +0700

    feat: add CommandLineRunner to init roles

--- src/main/java/com/enigmacamp/loan_app/config/BeanConfig.java
+++ src/main/java/com/enigmacamp/loan_app/config/BeanConfig.java
@@ -1,6 +1,10 @@
 package com.enigmacamp.loan_app.config;
 
+import com.enigmacamp.loan_app.constant.ERole;
+import com.enigmacamp.loan_app.entity.Role;
+import com.enigmacamp.loan_app.repository.RoleRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,6 +27,24 @@ public class BeanConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    CommandLineRunner initRoles(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByRole(ERole.ROLE_ADMIN).isEmpty()) {
+                Role admin = Role.builder().role(ERole.ROLE_ADMIN).build();
+                roleRepository.save(admin);
+            }
+            if (roleRepository.findByRole(ERole.ROLE_STAFF).isEmpty()) {
+                Role staff = Role.builder().role(ERole.ROLE_STAFF).build();
+                roleRepository.save(staff);
+            }
+            if (roleRepository.findByRole(ERole.ROLE_CUSTOMER).isEmpty()) {
+                Role customer = Role.builder().role(ERole.ROLE_CUSTOMER).build();
+                roleRepository.save(customer);
+            }
+        };
     }
 
 }
